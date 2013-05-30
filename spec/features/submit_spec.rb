7f7dd8594a092fb085a751ef3546ec0e4711f0f9
acacheung <acacheung@gmail.com>
2013-05-30 16:37:45 -0400

    delete spec controllers

---
+++ spec/features/submit_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "Project Submissions" do
+  # Must specify proj name, url, codebase url and a description
+  # Must specify valid url
+  # Must specify valid codebase url
+  # optional contact email for project
+  # if opt contact email specified, should validate is valid email
+
+
+end
