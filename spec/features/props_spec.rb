5e11d77fae7db13bddd794d193a1310c652d8313
Roger Camps <rogercampsny@gmail.com>
2013-06-02 18:34:16 -0400

    add validations for protfolio, prop and user models

--- spec/features/props_spec.rb
+++ spec/features/props_spec.rb
@@ -7,5 +7,13 @@ describe "Props" do
       visit props_path
       expect(page.status_code).to eql(200)
     end
+
+    it "cannot create props without the required fields" do
+      visit '/props/new'
+      fill_in "User", :with => ''
+      fill_in "Project", :with => ''
+      click_button "Create Prop"
+      page.should have_content("can't be blank")
+    end
   end
 end
