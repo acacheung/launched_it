14e3c3c31660ef57b88ad2e18fb0f46e1f9674fd
Roger Camps <rogercampsny@gmail.com>
2013-06-03 14:19:49 -0400

    added schema constraints to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :is_admin, :last_name, :password, :username
 
   validates_presence_of :email, :first_name, :last_name, :password, :username
-  validates :email, :format => { :with=>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, :allow_blank => true
+  validates :email, :format => { :with=>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -7,5 +7,16 @@ describe "Projects" do
       visit projects_path
       expect(page.status_code).to eql(200)
     end
+
+    it "creates a new project when all information is filled in" do
+      visit '/projects/new'
+      fill_in "Name", :with => ''
+      fill_in "Url", :with => ''
+      fill_in "Code base url", :with => ''
+      fill_in "Description", :with => ''
+      fill_in "Contact email", :with => ''
+      click_button "Create Project"
+      page.should have_content("can't be blank")
+    end
   end
 end
